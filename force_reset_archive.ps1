0504527648b7d49c8c1c57051633f52beb547187
elenajye0423-cmyk <elenajye0423@gmail.com>
2026-06-19 02:01:57 +0000

    삭제

--- force_reset_archive.ps1
+++
@@ -1,8 +0,0 @@
-﻿$utf8 = New-Object System.Text.UTF8Encoding $false
-
-$archiveSrc = [IO.File]::ReadAllText("portal_archive.html", $utf8)
-
-$archiveSrc = $archiveSrc -replace 'batech_archive_v2', 'batech_archive_v3'
-
-[IO.File]::WriteAllText("portal_archive.html", $archiveSrc, $utf8)
-
