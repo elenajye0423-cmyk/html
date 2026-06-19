b86581761c0c4b5a596b786d22aba5fdbf472b48
elenajye0423-cmyk <elenajye0423@gmail.com>
2026-06-20 00:26:15 +0900

    Add files via upload

---
+++ rename_file.ps1
@@ -0,0 +1,17 @@
+﻿$utf8 = New-Object System.Text.UTF8Encoding $false
+
+# 1. Rename the physical file to Korean
+if (Test-Path "leave_form.doc") {
+    Rename-Item "leave_form.doc" "연차_반차_휴가_신청서_양식.doc" -Force
+}
+
+# 2. Update portal_archive.html URL
+$archiveSrc = [IO.File]::ReadAllText("portal_archive.html", $utf8)
+$archiveSrc = $archiveSrc -replace 'url: "leave_form\.doc"', 'url: "연차_반차_휴가_신청서_양식.doc"'
+
+# Also bump the version string in the localStorage check to force a reset to pick up the new URL 
+# (though it might be better to just replace it directly in the DB if it exists)
+$archiveSrc = $archiveSrc -replace 'batech_archive_v3', 'batech_archive_v4'
+
+[IO.File]::WriteAllText("portal_archive.html", $archiveSrc, $utf8)
+
---
+++ update_portal_links.ps1
@@ -0,0 +1,24 @@
+﻿$utf8 = New-Object System.Text.UTF8Encoding $false
+$files = Get-ChildItem -Filter *.html
+
+foreach ($f in $files) {
+    if ($f.Name -eq "portal_notice.html" -or $f.Name -eq "portal_forms.html") {
+        continue
+    }
+    
+    $text = [IO.File]::ReadAllText($f.FullName, $utf8)
+    
+    # 1. Remove from GNB
+    $text = $text -replace '(?s)\s*<li class="nav-item">\s*<a href="portal_notice\.html">사내 포털</a>\s*<ul class="dropdown">\s*<li><a href="portal_notice\.html">사내 공지사항</a></li>\s*<li><a href="portal_forms\.html">서식 \(템플릿 및 자료실\)</a></li>\s*</ul>\s*</li>', ''
+    
+    # 2. Add to Footer Quick Links (append after map.html)
+    $text = $text -replace '<li><a href="map\.html">오시는 길</a></li>', '<li><a href="map.html">오시는 길</a></li>
+                    <li><a href="portal.html" style="color: var(--primary-light);">🔒 사내 포털</a></li>'
+    
+    [IO.File]::WriteAllText($f.FullName, $text, $utf8)
+}
+
+# Delete the old files
+if (Test-Path "portal_notice.html") { Remove-Item "portal_notice.html" }
+if (Test-Path "portal_forms.html") { Remove-Item "portal_forms.html" }
+
